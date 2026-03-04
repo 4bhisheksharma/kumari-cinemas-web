82e71aa91b6967972cd605bfed1a88fb7eb7b39f
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2026-03-04 13:14:18 +0545

    add: configure app settings and create User model

---
+++ models/User.cs
@@ -0,0 +1,8 @@
+public class User
+{
+    public int UserID { get; set; }
+    public required string UserName { get; set; }
+    public required string ContactNumber { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+}
